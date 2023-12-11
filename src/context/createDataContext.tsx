8f95b16728c38a4187ac6e409043b995b3086328
Neamul01 <neamulhaque2002@gmail.com>
2023-12-11 12:20:29 +0600

    type errors solved

--- src/context/createDataContext.tsx
+++ src/context/createDataContext.tsx
@@ -1,45 +1,44 @@
 import React, {createContext, useReducer, ReactNode, FC, Dispatch} from 'react';
 
-// Define generic types for the actions and the state
-interface ActionMap {
-  [key: string]: (dispatch: Dispatch<Action>) => (...args: any[]) => void;
-}
-
+// Define a generic Action type
 interface Action {
   type: string;
   [key: string]: any;
 }
 
-interface ContextProps<StateType, ActionType> {
-  state: StateType;
-  dispatch: Dispatch<ActionType>;
-}
+// Define a type for the actions object where each key is a function that returns an action creator function
+type ActionsMap<ActionType> = {
+  [key: string]: (dispatch: Dispatch<ActionType>) => (...args: any[]) => void;
+};
 
-// The Props interface defines the expected structure for the children props
+// Props interface for the Provider component
 interface Props {
   children: ReactNode;
 }
 
-// Create context with initial undefined value, which will be set in Provider
+// createDataContext now takes a generic ActionType
 function createDataContext<StateType, ActionType extends Action>(
   reducer: (state: StateType, action: ActionType) => StateType,
-  actions: ActionMap,
+  actions: ActionsMap<ActionType>,
   initialState: StateType,
 ) {
   const Context = createContext<
-    ContextProps<StateType, ActionType> | undefined
+    | {
+        state: StateType;
+        dispatch: Dispatch<ActionType>;
+      }
+    | undefined
   >(undefined);
 
   const Provider: FC<Props> = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    // Actions are processed to bind the dispatch function
+    // Bind actions to the dispatch function
     const boundActions: {[key: string]: (...args: any[]) => void} = {};
     for (let key in actions) {
       boundActions[key] = actions[key](dispatch);
     }
 
-    // The value prop expects an object with state and all bound actions
     return (
       <Context.Provider value={{state, dispatch, ...boundActions}}>
         {children}
